c345562eacc8fbb1022ef1e3aff77c2263b2c6e2
Julien Rialland <julien.rialland@gmail.com>
2011-07-11 10:05:18 +0000

    modified unit tests, so now we can pass a whole test using a mock servlet framework.
    git-svn-id: https://jfastcgi.svn.sourceforge.net/svnroot/jfastcgi/trunk@56 1081a822-4f85-4487-bc49-23d3bbc2ccd8

--- test/net/jr/fastcgi/test/SampleServer.java
+++ test/net/jr/fastcgi/test/SampleServer.java
@@ -2,26 +2,41 @@ package net.jr.fastcgi.test;
 
 import com.fastcgi.FCGIInterface;
 
-public class SampleServer implements Runnable{
+/**
+ * A dummy fastcgi application in java.
+ * 
+ * @author jrialland
+ *
+ */
+public class SampleServer implements Runnable {
 
 	public static final int DEFAULT_PORT = 59812;
-	
-	private int port = DEFAULT_PORT;
-		
-	public void run(){
+
+	private int port;
+
+	public SampleServer(int port) {
+		this.port = port;
+	}
+
+	public SampleServer() {
+		this(DEFAULT_PORT);
+	}
+
+	/**
+	 * Answers request by sending back an html page.
+	 */
+	public void run() {
 		int count = 0;
-		System.setProperty("FCGI_PORT", ""+port);
+		System.setProperty("FCGI_PORT", "" + port);
 		FCGIInterface itf = new FCGIInterface();
 		while (itf.FCGIaccept() >= 0) {
 			count++;
 			System.out.println("Content-type: text/html\n\n");
 			System.out.println("<html>");
-			System.out
-					.println("<head><TITLE>FastCGI-Hello Java stdio</TITLE></head>");
+			System.out.println("<head><TITLE>FastCGI-Hello Java stdio</TITLE></head>");
 			System.out.println("<body>");
 			System.out.println("<H3>FastCGI-HelloJava stdio</H3>");
-			System.out.println("request number " + count + " running on host "
-					+ System.getProperty("SERVER_NAME"));
+			System.out.println("request number " + count + " running on host " + System.getProperty("SERVER_NAME"));
 			System.out.println("</body>");
 			System.out.println("</html>");
 		}
@@ -34,5 +49,17 @@ public class SampleServer implements Runnable{
 	public void setPort(int port) {
 		this.port = port;
 	}
-	
+
+	/**
+	 * it accepts a tcp port number to bind as parameter
+	 * @param args
+	 */
+	public static void main(String... args) {
+		int port = DEFAULT_PORT;
+		// serve forever
+		if (args.length > 0) {
+			port = Integer.valueOf(args[0]);
+		}
+		new SampleServer(port).run();
+	}
 }
