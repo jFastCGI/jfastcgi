1b09603db9220ebcee2c1fddae9f3dd9d9529398
Dominik Dorn <dominik.dorn@gmail.com>
2013-08-09 13:43:11 +0200

    refs #5 added Apache 2 License header

--- server/src/main/java/com/fastcgi/FCGIRequest.java
+++ server/src/main/java/com/fastcgi/FCGIRequest.java
@@ -1,33 +1,35 @@
 /*
- * @(#)FCGIRequest.java
- *
- *  FastCGi compatibility package Interface
- *
- *  Copyright (c) 1996 Open Market, Inc.
- *
- * See the file "LICENSE.TERMS" for information on usage and redistribution
- * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
- *
- * $Id: FCGIRequest.java,v 1.3 2000/03/21 12:12:26 robs Exp $
- */
+ Copyright (c) 1996 Open Market, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 package com.fastcgi;
 
-import java.net.*;
-import java.io.FileDescriptor;
+import java.net.Socket;
 import java.util.Properties;
 
-public class FCGIRequest 
-{
-    private static final String RCSID = "$Id: FCGIRequest.java,v 1.3 2000/03/21 12:12:26 robs Exp $";
+public class FCGIRequest {
 
-    /* This class has no methods. Right now we are single threaded
+    /*
+    * This class has no methods. Right now we are single threaded
     * so there is only one request object at any given time which
-    * is refrenced by an FCGIInterface class variable . All of this
+    * is referenced by an FCGIInterface class variable . All of this
     * object's data could just as easily be declared directly there.
     * When we thread, this will change, so we might as well use a
-    * seperate class. In line with this thinking, though somewhat
+    * separate class. In line with this thinking, though somewhat
     * more perversely, we kept the socket here.
     */
+
     /*
      * class variables
      */
@@ -37,17 +39,17 @@ public class FCGIRequest
     /*
      * instance variables
      */
-    public Socket       socket;
-    public boolean      isBeginProcessed;
-    public int      requestID;
-    public boolean      keepConnection;
-    public int      role;
-    public int      appStatus;
-    public int      numWriters;
-    public FCGIInputStream  inStream;
+    public Socket socket;
+    public boolean isBeginProcessed;
+    public int requestID;
+    public boolean keepConnection;
+    public int role;
+    public int appStatus;
+    public int numWriters;
+    public FCGIInputStream inStream;
     public FCGIOutputStream outStream;
     public FCGIOutputStream errStream;
-    public Properties   params;
+    public Properties params;
 }
 
 
