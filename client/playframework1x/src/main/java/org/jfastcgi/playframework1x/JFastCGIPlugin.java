e10da18cf237b0ca30c8e786377799263af558f0
Dominik Dorn <dominik.dorn@gmail.com>
2013-08-19 00:35:07 +0200

    refs #10: PlayFramework 1.x integration - plugin configuration
    first draft of plugin configuration incl. documentation

--- client/playframework1x/src/main/java/org/jfastcgi/playframework1x/JFastCGIPlugin.java
+++ client/playframework1x/src/main/java/org/jfastcgi/playframework1x/JFastCGIPlugin.java
@@ -1,17 +1,57 @@
 package org.jfastcgi.playframework1x;
 
+import play.Play;
 import play.PlayPlugin;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * jFastCGI Plugin for PlayFramework 1.x (>= 1.2.6)
  */
 public class JFastCGIPlugin extends PlayPlugin {
-    /**
-     * TODO
-     *  - read configuration from application.conf
-     *  - CREATE connection pool
+    /*
+
+     jfastcgi.DEFAULT.server-address = 127.0.0.1:19000
+     jfastcgi.DEFAULT.start-executable = "/usr/bin/php-fcgi -b 19000"
+     ...
+     jfastcgi.ALTERNATE.server-address = 127.0.0.1:19000
+     jfastcgi.ALTERNATE.connection-factory = com.yourcompany.fastcgi.CustomConnectionFactory
+     ...
+     jfastcgi.ACLUSTERCONFIG.cluster-adresses = 127.0.0.1:19000,[::1]:20000,[::1]:21000
+     ...
+     jfastcgi.FILTERINGHEADERS.server-address = 127.0.0.1:19000
+     jfastcgi.FILTERINGHEADERS.filtered-headers = Authorization;
      */
+    private final Pattern configPattern = Pattern.compile("^jfastcgi.(.*).(server-address|cluster-adresses|start-executable|connection-factory|filtered-headers|context-path)$");
 
+    @Override
+    public void onApplicationStart() {
 
+        Set<Object> keys = Play.configuration.keySet();
 
+        for (Object key : keys) {
+            Matcher matcher = configPattern.matcher(key.toString());
+            if(matcher.matches()){
+                String endpointName = matcher.group(1);
+                String paramName = matcher.group(2);
+                String value = Play.configuration.getProperty((String) key);
+
+                // store into configuration object
+            }
+        }
+
+        // iterate over all configuration objects
+        // start connections
+
+    }
+
+    @Override
+    public void onApplicationStop() {
+        // iterate over all configuration objects
+        // close connections
+        // remove configuration objects from memory
+
+    }
 }
