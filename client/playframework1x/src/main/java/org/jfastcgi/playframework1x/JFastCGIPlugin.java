cc474c7bb548a9c58c965545a481416313e90764
Dominik Dorn <dominik.dorn@gmail.com>
2013-08-20 00:00:55 +0200

    refs #10 parsing of configurations.

--- client/playframework1x/src/main/java/org/jfastcgi/playframework1x/JFastCGIPlugin.java
+++ client/playframework1x/src/main/java/org/jfastcgi/playframework1x/JFastCGIPlugin.java
@@ -1,8 +1,13 @@
 package org.jfastcgi.playframework1x;
 
+import org.jfastcgi.client.FastCGIHandler;
+import org.jfastcgi.client.FastCGIHandlerFactory;
 import play.Play;
 import play.PlayPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,21 +31,53 @@ public class JFastCGIPlugin extends PlayPlugin {
      */
     private final Pattern configPattern = Pattern.compile("^jfastcgi.(.*).(server-address|cluster-adresses|start-executable|connection-factory|filtered-headers|context-path)$");
 
-    @Override
-    public void onApplicationStart() {
+    private Properties _configuration = null;
 
-        Set<Object> keys = Play.configuration.keySet();
+    private Properties getConfiguration() {
+        if (_configuration == null) {
+            return Play.configuration;
+        }
+        else {
+            return _configuration;
+        }
+    }
+
+
+
+    Map<String, Map<String, String>> createConfigMap(Properties configuration) {
+        Set<Object> keys = configuration.keySet();
+
+        Map<String, Map<String, String>> configMap = new HashMap<String, Map<String, String>>();
 
         for (Object key : keys) {
             Matcher matcher = configPattern.matcher(key.toString());
-            if(matcher.matches()){
+            if (matcher.matches()) {
                 String endpointName = matcher.group(1);
                 String paramName = matcher.group(2);
-                String value = Play.configuration.getProperty((String) key);
+                String value = configuration.getProperty((String) key);
 
-                // store into configuration object
+                if (!configMap.containsKey(endpointName)) {
+                    configMap.put(endpointName, new HashMap<String, String>());
+                }
+
+                configMap.get(endpointName).put(paramName, value);
             }
         }
+        return configMap;
+    }
+
+    Map<String, FastCGIHandler> createHandlers(Map<String, Map<String, String>> configMap) {
+        Map<String, FastCGIHandler> result = new HashMap<String, FastCGIHandler>();
+        for (String endpointName : configMap.keySet()) {
+            result.put(endpointName, FastCGIHandlerFactory.create(configMap.get(endpointName)));
+        }
+        return result;
+    }
+
+    @Override
+    public void onApplicationStart() {
+        Map<String, Map<String, String>> configMap = createConfigMap(getConfiguration());
+
 
         // iterate over all configuration objects
         // start connections
@@ -54,4 +91,10 @@ public class JFastCGIPlugin extends PlayPlugin {
         // remove configuration objects from memory
 
     }
+
+
+    public void setConfiguration(Properties properties) {
+        this._configuration = properties;
+    }
+
 }
---
+++ client/playframework1x/src/test/java/org/jfastcgi/playframework1x/JFastCGIPluginTest.java
@@ -0,0 +1,62 @@
+package org.jfastcgi.playframework1x;
+
+import org.jfastcgi.client.FastCGIHandler;
+import org.junit.Before;
+import org.junit.Test;
+import play.Play;
+
+import java.util.Map;
+import java.util.Properties;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Test for the JFastCGIPlugin for PlayFramework 1x
+ */
+public class JFastCGIPluginTest {
+    Properties conf;
+    @Before
+    public void setUp() throws Exception {
+
+        conf = new Properties();
+        conf.setProperty("jfastcgi.DEFAULT.server-address","127.0.0.1:19000");
+        conf.setProperty("jfastcgi.DEFAULT.start-executable","/usr/bin/php-fcgi -b 19000");
+        conf.setProperty("jfastcgi.ALTERNATE.server-address","127.0.0.1:19000");
+        conf.setProperty("jfastcgi.ALTERNATE.connection-factory","com.yourcompany.fastcgi.CustomConnectionFactory");
+        conf.setProperty("jfastcgi.ACLUSTERCONFIG.cluster-adresses","127.0.0.1:19000;[::1]:20000;[::1]:21000");
+        conf.setProperty("jfastcgi.FILTERINGHEADERS.server-address","127.0.0.1:19000");
+        conf.setProperty("jfastcgi.FILTERINGHEADERS.filtered-headers","Authorization;");
+        conf.setProperty("jfastcgi.CONTEXTOVERRIDEEXAMPLE.server-address","127.0.0.1:19000");
+        conf.setProperty("jfastcgi.CONTEXTOVERRIDEEXAMPLE.context-path","/someapp");
+    }
+
+
+    @Test
+    public void testCreateConfigMap() throws Exception {
+        JFastCGIPlugin plugin = new JFastCGIPlugin();
+        plugin.setConfiguration(conf);
+
+        Map<String, Map<String, String>> configMap = plugin.createConfigMap(conf);
+
+        assertThat(configMap).isNotNull();
+        assertThat(configMap).hasSize(5);
+        assertThat(configMap.containsKey("DEFAULT"));
+        assertThat(configMap.get("DEFAULT")).hasSize(2);
+        assertThat(configMap.get("ACLUSTERCONFIG")).hasSize(1);
+        assertThat(configMap.get("FILTERINGHEADERS")).hasSize(2);
+        assertThat(configMap.get("CONTEXTOVERRIDEEXAMPLE")).hasSize(2);
+    }
+
+    @Test
+    public void testCreateHandlers() throws Exception {
+        JFastCGIPlugin plugin = new JFastCGIPlugin();
+        plugin.setConfiguration(conf);
+
+        Map<String, Map<String, String>> configMap = plugin.createConfigMap(conf);
+        Map<String, FastCGIHandler> handlers = plugin.createHandlers(configMap);
+
+        assertThat(handlers).isNotNull().hasSize(5);
+
+
+    }
+}
