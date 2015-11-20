2c46f8ec53cc64ae0c057f1df769115637571845
Julien.Rialland <julien.rialland@gmail.com>
2015-11-20 10:59:04 +0100

    fixed smoke test -> the test passed now

--- client/servlet/src/test/java/org/jfastcgi/servlets/test/SampleServer.java
+++ client/servlet/src/test/java/org/jfastcgi/servlets/test/SampleServer.java
@@ -19,7 +19,13 @@
 */
 package org.jfastcgi.servlets.test;
 
-import com.fastcgi.FCGIInterface;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * A dummy fastcgi application in java.
@@ -28,57 +34,135 @@ import com.fastcgi.FCGIInterface;
  */
 public class SampleServer implements Runnable {
 
-    public static final int DEFAULT_PORT = 59812;
+	private int port;
 
-    private int port;
+	private static enum PacketType {
+		FCGI_STDOUT(6), FCGI_END_REQUEST(3);
 
-    public SampleServer(int port) {
-        this.port = port;
-    }
+		int code;
 
-    public SampleServer() {
-        this(DEFAULT_PORT);
-    }
+		PacketType(int code) {
+			this.code = code;
+		}
 
-    /**
-     * Answers request by sending back an html page.
-     */
-    public void run() {
-        int count = 0;
-        System.setProperty("FCGI_PORT", "" + port);
-        FCGIInterface itf = new FCGIInterface();
-        while (itf.FCGIaccept() >= 0) {
-            count++;
-            System.out.println("Content-type: text/html\n\n");
-            System.out.println("<html>");
-            System.out.println("<head><TITLE>FastCGI-Hello Java stdio</TITLE></head>");
-            System.out.println("<body>");
-            System.out.println("<H3>FastCGI-HelloJava stdio</H3>");
-            System.out.println("request number " + count + " running on host " + System.getProperty("SERVER_NAME"));
-            System.out.println("</body>");
-            System.out.println("</html>");
-        }
-    }
+		public int getCode() {
+			return code;
+		}
+	}
 
-    public int getPort() {
-        return port;
-    }
+	public SampleServer(int port) {
+		this.port = port;
 
-    public void setPort(int port) {
-        this.port = port;
-    }
+	}
 
-    /**
-     * it accepts a tcp port number to bind as parameter
-     *
-     * @param args
-     */
-    public static void main(String... args) {
-        int port = DEFAULT_PORT;
-        // serve forever
-        if (args.length > 0) {
-            port = Integer.valueOf(args[0]);
-        }
-        new SampleServer(port).run();
-    }
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * <pre>
+	 * 		typedef struct {
+	 * 		    unsigned char version;
+	 * 		    unsigned char type;
+	 * 		    unsigned char requestIdB1;
+	 * 		    unsigned char requestIdB0;
+	 * 		    unsigned char contentLengthB1;
+	 * 		    unsigned char contentLengthB0;
+	 * 		    unsigned char paddingLength;
+	 * 		    unsigned char reserved;
+	 * 		} FCGI_Header;
+	 * </pre>
+	 * 
+	 * @param os
+	 */
+	private void writeHeader(DataOutputStream os, PacketType pkt, int requestId, int contentLength) throws IOException {
+		os.write(1);// version
+		os.write(pkt.getCode());// type
+		os.writeShort(requestId);
+		os.writeShort(contentLength);// contentLength
+		os.write(0);// paddingLength
+		os.write(0);// reserved
+	}
+
+	private void writeStdout(DataOutputStream os, int requestId, String content) throws IOException {
+		writeHeader(os, PacketType.FCGI_STDOUT, requestId, content.length());
+		os.write(content.getBytes());
+	}
+
+	/**
+	 * <pre>
+	 * typedef struct {
+	 *     unsigned char appStatusB3;
+	 *     unsigned char appStatusB2;
+	 *     unsigned char appStatusB1;
+	 *     unsigned char appStatusB0;
+	 *     unsigned char protocolStatus;
+	 *     unsigned char reserved[3];
+	 * } FCGI_EndRequestBody;
+	 * </pre>
+	 * 
+	 * @param os
+	 */
+	private void writeEndRequest(DataOutputStream os, int requestId, int status) throws IOException {
+		writeHeader(os, PacketType.FCGI_END_REQUEST, requestId, 8);
+		os.write(0);// appStatusB3
+		os.write(0);// appStatusB2
+		os.write(0);// appStatusB1
+		os.write(0);// appStatusB0
+		os.write(status);// protocolStatus
+		os.write(new byte[3]);// reserved
+	}
+
+	public void run() {
+		ServerSocket ss = null;
+		try {
+			ss = new ServerSocket();
+			ss.bind(new InetSocketAddress("localhost", port));
+
+			Socket client;
+			while ((client = ss.accept()) != null) {
+
+				DataInputStream in = new DataInputStream(client.getInputStream());
+				in.skip(2);
+				int requestId = in.readShort();
+
+				final DataOutputStream os = new DataOutputStream(client.getOutputStream());
+
+				StringWriter sw = new StringWriter();
+				sw.append("Content-type: text/html\r\n\r\n");
+				sw.append("<html>");
+				sw.append("<head><title>FastCGI-Hello</title></head>");
+				sw.append("<body>");
+				sw.append("<h3>Hello from fcgi !</h3>");
+				sw.append("</body>");
+				sw.append("<html>");
+
+				writeStdout(os, requestId, sw.toString());
+				writeStdout(os, requestId, "");
+				writeEndRequest(os, requestId, 0);
+			}
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				if (ss != null) {
+					ss.close();
+				}
+			} catch (IOException ioe) {
+				throw new RuntimeException(ioe);
+			}
+		}
+	}
+
+	public Thread start() {
+		Thread t = new Thread(this);
+		t.setDaemon(true);
+		t.start();
+		return t;
+	}
+
+	public static void main(String[] args) {
+		new SampleServer(8541).run();
+	}
 }
--- client/servlet/src/test/java/org/jfastcgi/servlets/test/TestFastCgiConnection.java
+++ client/servlet/src/test/java/org/jfastcgi/servlets/test/TestFastCgiConnection.java
@@ -19,77 +19,117 @@
 */
 package org.jfastcgi.servlets.test;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.jfastcgi.client.FastCGIHandlerFactory;
+import org.jfastcgi.servlet.FastCGIServlet;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.meterware.httpunit.GetMethodWebRequest;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
-import org.jfastcgi.servlet.FastCGIServlet;
-import org.jfastcgi.client.FastCGIHandlerFactory;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import javax.servlet.http.HttpServletResponse;
-
-import static junit.framework.Assert.assertEquals;
 
 public class TestFastCgiConnection {
-    private FastCGIServlet fastCGIServlet;
-    private SampleServer sampleServer = new SampleServer();
 
-    private Thread sampleServerThread = null;
-    private Server jettyServer = null;
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestFastCgiConnection.class);
 
-    @Before
-    public void setUp() throws Exception {
-        // start dummy fastcgi app
-        sampleServerThread = new Thread(sampleServer);
-        sampleServerThread.setDaemon(true);
-        sampleServerThread.start();
+	private Thread fastCgiServer;
 
+	int serverPort = 0;
 
-        jettyServer = new Server(8080);
+	int fcgiPort = 0;
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        jettyServer.setHandler(context);
+	private Server jettyServer = null;
 
-        ServletHolder holder = context.addServlet(FastCGIServlet.class, "/*");
-        holder.setInitParameter(FastCGIHandlerFactory.PARAM_SERVER_ADDRESS, "localhost:" + sampleServer.getPort());
+	public static List<Integer> getFreeTcpPorts(int amount) throws Exception {
+		List<Integer> list = new ArrayList<Integer>(amount);
+		List<ServerSocket> toClose = new ArrayList<ServerSocket>(amount);
+		try {
+			for (int i = 0; i < amount; i++) {
+				final ServerSocket ss = new ServerSocket(0);
+				list.add(ss.getLocalPort());
+				toClose.add(ss);
+			}
+		} finally {
+			for (ServerSocket ss : toClose) {
+				ss.close();
+			}
+		}
+		return list;
+	}
 
-        jettyServer.start();
-//        jettyServer.join();
+	@Before
+	public void setUp() throws Exception {
 
-    }
+		List<Integer> freePorts = getFreeTcpPorts(2);
+		fcgiPort = freePorts.get(0);
+		serverPort = freePorts.get(1);
 
-    //    @Override
-    @After
-    public void tearDown() throws Exception {
-        // stop fastcgi thread
-        sampleServerThread.interrupt();
+		LOGGER.debug("Starting fcgi server");
+		fastCgiServer = new SampleServer(fcgiPort).start();
 
-        // stop jetty
-        jettyServer.stop();
-    }
+		LOGGER.debug("fcgi server started");
 
-    /**
-     * Simple test : just send a request, and assert that the answer is ok
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testItWorks() throws Exception {
-        WebConversation wc = new WebConversation();
+		LOGGER.debug("Starting servlet container");
+		serverPort = 8080;
+		jettyServer = new Server(serverPort);
 
-        WebRequest request = new GetMethodWebRequest("http://localhost:8080/");
-        WebResponse response = wc.getResponse(request);
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setResourceBase(Files.createTempFile("jetty", "").toAbsolutePath().toString());
+		context.setContextPath("/");
+		jettyServer.setHandler(context);
 
-        assertEquals(response.getResponseCode(), HttpServletResponse.SC_OK);
-        assertEquals(response.getElementsByTagName("H3").length, 1);
-        assertEquals(response.getTitle(), "FastCGI-Hello Java stdio");
-    }
+		ServletHolder holder = context.addServlet(FastCGIServlet.class, "/*");
+		holder.setInitParameter(FastCGIHandlerFactory.PARAM_SERVER_ADDRESS, "localhost:" + fcgiPort);
+
+		jettyServer.start();
+		LOGGER.debug("Servlet container started, listening on port " + serverPort);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// stop jetty
+		jettyServer.stop();
+
+		// stop fastcgi server
+		fastCgiServer.interrupt();
+	}
+
+	/**
+	 * Simple test : just send a request, and assert that the answer is ok
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testItWorks() throws Exception {
+
+		WebConversation wc = new WebConversation();
+
+		WebRequest request = new GetMethodWebRequest("http://localhost:" + serverPort + "/");
+		WebResponse response = wc.getResponse(request);
+		System.out.println(new String(response.getBytes()));
+
+		assertEquals(response.getResponseCode(), HttpServletResponse.SC_OK);
+		assertTrue(response.getContentLength() > 0);
+
+		assertEquals(response.getElementsByTagName("H3").length, 1);
+		assertEquals(response.getTitle(), "FastCGI-Hello");
+	}
 
 }
